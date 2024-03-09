c14e8bd491fe3cc25462f5eedc778113848004bd
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2024-03-10 04:13:21 +0530

    implemention of `calcy_parser_brackets` and `calcy_parser_functions`

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,10 +1,10 @@
 #include "./parser.h"
 
-bool calcy_parser_init(calcy_parser *parser, const calcy_token *toks)
+bool calcy_parser_init(calcy_parser *parser, const calcy_token *toks_start)
 {
     if (!parser)
         return false;
-    parser->M_tokens = toks;
+    parser->M_tokens = toks_start;
     parser->M_current_parser = 0;
     return true;
 }
@@ -59,13 +59,75 @@ DATA_TYPE calcy_parser_expo(calcy_parser *parser)
 {
     if (!parser)
         return INFINITY;
-    DATA_TYPE res = calcy_parser_factor(parser);
+    DATA_TYPE res = calcy_parser_functions(parser);
     while (parser->M_tokens[parser->M_current_parser].M_type == TOKEN_OPERATOR_EXPONENTIAL)
     {
         parser->M_current_parser++;
-        res = powl(res, calcy_parser_factor(parser));
+        res = powl(res, calcy_parser_functions(parser));
     }
     return res;
+}
+
+DATA_TYPE calcy_parser_functions(calcy_parser *parser)
+{
+    if (!parser)
+        return INFINITY;
+    if (parser->M_tokens[parser->M_current_parser].M_type == TOKEN_ALPHA)
+    {
+        while (parser->M_tokens[parser->M_current_parser].M_type == TOKEN_ALPHA)
+        {
+            const char *function_name = parser->M_tokens[parser->M_current_parser++].M_lexeme;
+            DATA_TYPE in_val = calcy_parser_brackets(parser);
+            if (strcmp(function_name, "sin") == 0)
+            {
+                return sinl(in_val);
+            }
+            else if (strcmp(function_name, "e") == 0)
+            {
+                return M_E;
+            }
+            else if (strcmp(function_name, "pi") == 0 || strcmp(function_name, "PI") == 0)
+            {
+                return M_PI;
+            }
+            else if (strcmp(function_name, "sqrt") == 0)
+            {
+                return sqrtl(in_val);
+            }
+            else if (strcmp(function_name, "cbrt") == 0)
+            {
+                return cbrtl(in_val);
+            }
+        }
+    }
+    else if (parser->M_tokens[parser->M_current_parser].M_type == TOKEN_NUMBER || parser->M_tokens[parser->M_current_parser].M_type == TOKEN_LEFT_PAREN)
+    {
+        return calcy_parser_brackets(parser);
+    }
+    return INFINITY;
+}
+
+DATA_TYPE calcy_parser_brackets(calcy_parser *parser)
+{
+    if (!parser)
+        return INFINITY;
+    if (parser->M_tokens[parser->M_current_parser].M_type == TOKEN_LEFT_PAREN)
+    {
+        parser->M_current_parser++;
+        calcy_parser ptemp;
+        ptemp.M_current_parser = 0;
+        ptemp.M_tokens = parser->M_tokens + parser->M_current_parser;
+        DATA_TYPE result = calcy_parser_perform(&ptemp);
+        parser->M_current_parser += ptemp.M_current_parser;
+        if (parser->M_tokens[parser->M_current_parser].M_type == TOKEN_RIGHT_PAREN)
+            parser->M_current_parser++;
+        return result;
+    }
+    else if (parser->M_tokens[parser->M_current_parser].M_type == TOKEN_NUMBER)
+    {
+        return calcy_parser_factor(parser);
+    }
+    return INFINITY;
 }
 
 DATA_TYPE calcy_parser_factor(calcy_parser *parser)
