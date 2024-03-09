c83c2abebbf0abbc644e67f065352e33e54747a5
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2024-03-10 04:11:56 +0530

    added tokens for function names and equal to

--- src/token_type/token_type.h
+++ src/token_type/token_type.h
@@ -4,13 +4,15 @@
 typedef enum token_type
 {
     TOKEN_NUMBER,               // Represents numbers (1, 3.14, ...)
+    TOKEN_ALPHA,                // Represents functions (sin, cos, ...)
     TOKEN_OPERATOR_ADD,         // Represents addition operator (+)
     TOKEN_OPERATOR_SUBSTRACT,   // Represents substraction (-)
     TOKEN_OPERATOR_MULTIPLY,    // Represents multiplication (*)
     TOKEN_OPERATOR_DIVIDE,      // Represents division (/)
     TOKEN_OPERATOR_EXPONENTIAL, // Represents exponential (^)
-    TOKEN_RIGHT_PAREN,          // Represents right parenthesis )
     TOKEN_LEFT_PAREN,           // Represents left parenthesis (
+    TOKEN_RIGHT_PAREN,          // Represents right parenthesis )
+    TOKEN_EQUAL_TO,             // Represents equal to (=)
     TOKEN_EOF                   // Represents END OF FILE
 } calcy_token_type;
 
